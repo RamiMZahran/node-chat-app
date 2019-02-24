6407e3b3645888654f2f03a93872c7f3946c1f67
RamiMZahran <ramimohamedsalah96@gmail.com>
2019-02-24 19:37:56 +0200

    Added Form and send button to send and show msgs on html

--- server/server.js
+++ server/server.js
@@ -20,9 +20,10 @@ io.on('connection', (socket) => {
 
     socket.broadcast.emit('newMessage',generateMsg('Admin','New user joined'));
 
-    socket.on('createMessage', (msg) => {
+    socket.on('createMessage', (msg,callback) => {
         console.log('createMessage', msg);
         io.emit('newMessage', generateMsg(msg.from,msg.text));
+        callback('This is from the server.');
         // socket.broadcast.emit('newMessage', {
         //         from: msg.from,
         //         text: msg.text,
